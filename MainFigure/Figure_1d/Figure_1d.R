f93c0e39a9a557a84ef451b8f62422be281b5db3
MaggieQI <23248016+StumbleMQ@users.noreply.github.com>
2025-01-08 10:20:18 +0800

    Update Figure_1d.R and Supple.Figure_8a

--- MainFigure/Figure_1d/Figure_1d.R
+++ MainFigure/Figure_1d/Figure_1d.R
@@ -9,7 +9,8 @@ library(data.table)
 ###
 getwd()
 # setwd("~/Downloads/NC_figure_20250102/Figure_1d")
-###########################################################################
+# This file contains codes for Figure_1d and Supple.Figure_8a
+##############################################################################################
 ### The file named "BMDM2024_gseaFigures.csv" contains annotated genes of all samples 
 ### BMDM cells collected from wild type Mcoln1 mice subjected to LPS with ML-SA5 treatment group: "LPSA5.1" "LPSA5.2" "LPSA5.3" "LPSA5.4"
 ### BMDM cells collected from wild type Mcoln1 mice subjected to LPS without ML-SA5 treatment group::"LPS.1", "LPS.2", "LPS.3", "LPS.4"  
@@ -21,11 +22,10 @@ colnames(rawcounts)
 colnames(rawcounts) <- c("GeneName","LPSA5.1", "LPSA5.2", "LPSA5.3", "LPSA5.4", "LPS.1","LPS.2","LPS.3","LPS.4")
 View(rawcounts)
 
-## select distinct Gene Name row.
+# select distinct Gene Name row.
 rawcounts.1 <- rawcounts %>%
     distinct(GeneName, .keep_all = T)
 dim(rawcounts.1) # [1] 51732     9
-
 ### 
 write.csv(rawcounts.1, file = "BMDMLPS_rawcounts.csv", row.names = FALSE)
 ### genes names error : march1 should be Marchf1.
@@ -63,7 +63,7 @@ pheatmap(sampleDistMatrix,
          col = colors)
 ## PCA Analysis
 plotPCA(rld,intgroup=c("treated"))
-############ Differential analysis ######################################
+############ Differential analysis ###################
 dds <- DESeq(dds) 
 # estimating size factors
 # estimating dispersions
@@ -72,7 +72,7 @@ dds <- DESeq(dds)
 # final dispersion estimates
 # fitting model and testing
 
-################## ###################
+################## ######################################
 res1 <- results(dds,tidy=TRUE)
 View(res1)
 #### BMDMLPS_DESeq2_deg.csv indicates the differential genes expression: 
@@ -118,7 +118,8 @@ colnames(deg_BMDMLPS)
 # [5] "lfcSE"          "stat"           "pvalue"         "padj" 
 colnames(deg_BMDMLPS) <- c("GeneName", "baseMean","log2FoldChange", "lfcSE","stat","pvalue", "padj")
 View(deg_BMDMLPS)
-## Transfer mouse gene names to human gene names
+
+# Transfer mouse gene names to human gene names
 deg_BMDMLPS$GeneName <- toupper(deg_BMDMLPS$GeneName)
 
 ### build up genes expression lists 
@@ -137,7 +138,7 @@ View(f1)
 ## select log2FoldChange and pvalue as parameters to build the differential expressed genes list.
 exp.fc=f1[,c(1,3,6)]
 View(exp.fc)
-##############  id转化##################################
+##############  id transform ################
 library(msigdbr)
 library(GSEABase)
 library(clusterProfiler)
@@ -157,11 +158,8 @@ head(exp.fc.id, 3)
 # 2     AAAS     0.06253631 0.5775995916     8086
 
 dim(exp.fc.id) #[1] 10805     4
-
 exp.fc.id=na.omit(exp.fc.id) 
 View(exp.fc.id) 
-
-## Arrange the genes list by log2FoldChange###################################
 ################ Arrange the gene list by the value of log2FoldChange #########
 exp.fc.id.sorted <- exp.fc.id[order(exp.fc.id$log2FoldChange, decreasing = T),]
 head(exp.fc.id.sorted, 2)
@@ -175,17 +173,7 @@ head(id.fc,2)
 # 5.133285 5.009726 
 View(id.fc)
 gsea <- gseKEGG(id.fc, organism = "hsa",pvalueCutoff = 0.5)
-?gseKEGG
-
-#Reading KEGG annotation online: "https://rest.kegg.jp/link/hsa/pathway"...
-#Reading KEGG annotation online: "https://rest.kegg.jp/list/pathway/hsa"...
-#preparing geneSet collections...
-#GSEA analysis...
-#leading edge analysis...
-# done...
-# There were 15 warnings (use warnings() to see them)
-
-#### Check Result
+### Check Result ###
 gseKEGG <- gsea@result
 write.table(gseKEGG,"deg_BMDMLPS_enrich_gseKEGG.txt",sep = "\t",col.names = NA)
 View(gseKEGG)
@@ -203,35 +191,27 @@ gseaplot2(gsea, geneSetID = "hsa05321",
           subplots = 1:3,
           pvalue_table = TRUE,
           ES_geom = "line")
-
-?gseaplot2
-
 #######################################################################################
-
-## Arrange the genes list by score 
-# score =(-log10(pValue)*SIGN(log2Foldchange))
+### Alternatively arrange the genes list by score 
+### score =(-log10(pValue)*SIGN(log2Foldchange))
 View(exp.fc.id)
-###############
+###
 exp.fc.id.score <- exp.fc.id
 View(exp.fc.id.score)
 exp.fc.id.score$score = (-log10(exp.fc.id.score$pValue)*sign(exp.fc.id.score$log2Foldchange))
 # Error in log10(exp.fc.id.score$pValue) :  non-numeric argument to mathematical function
-## manually construct score column using excel 
 
+### manually construct score column using excel Or using the following code to build a score ranking file
 exp.fc.id.score <- exp.fc.id.score[order(exp.fc.id.score$pvalue, decreasing = F),]
 head(exp.fc.id.score, 5)
 
-### sign in a log vale into "0" in the first four gene name
+###  sign in a relevant vale into "0" in the first four genes name because log10(0) gives a N/A value
 exp.fc.id.score$pvalue[c(1:4)] <- c(6.0e-298,6.0e-298,6.0e-298,6.0e-298)
-
 head(exp.fc.id.score, 10)
-
 library(dbplyr)
 library(data.table)
-
 exp.fc.id.score$score = (-log10(exp.fc.id.score$pvalue)*sign(exp.fc.id.score$log2FoldChange))
-################ Arrange the gene list by the value of log2FoldChange #########
-
+################ Arrange the gene list by the value of score ############################
 exp.fc.id.score.sorted <- exp.fc.id.score[order(exp.fc.id.score$score, decreasing = T),]
 head(exp.fc.id.score.sorted, 8)
 #     GeneName log2FoldChange        pvalue ENTREZID    score
@@ -247,19 +227,11 @@ head(id.fc.score,2)
 
 View(id.fc.score)
 gsea <- gseKEGG(id.fc.score, organism = "hsa",pvalueCutoff = 0.5)
-?gseKEGG
 
-# preparing geneSet collections...
-# GSEA analysis...
-# leading edge analysis...
-# done...
-# There were 14 warnings (use warnings() to see them)
-
-#### check result
+### check result ###
 gseKEGG <- gsea@result
 write.table(gseKEGG,"deg_BMDMLPS_enrich_score_gseKEGG.txt",sep = "\t",col.names = NA)
 View(gseKEGG)
-
 gseaplot2(gsea, geneSetID = "hsa04216",
           title = "Ferroptosis",
           color ="black",
@@ -272,8 +244,8 @@ gseaplot2(gsea, geneSetID = "hsa05321",
           subplots = 1:3,
           pvalue_table = TRUE,
           ES_geom = "line")
-
-### Alternatively using the "result" file to build GSEAPreranked files 
-### using GSEA software ( Broad Institue GSEA v4.1.3 and GSEA v4.3.3 gene set enrichment analysis)for GSEA analysis. 
-################################################################################
+### Alternatively using the "result" file to build GSEAPreranked files, 
+### using GSEA software ( Broad Institue GSEA v4.1.3 and
+### GSEA v4.3.3 gene set enrichment analysis)for GSEA analysis. 
+###################################### END ############################
 
