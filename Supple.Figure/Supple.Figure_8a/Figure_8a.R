1a601dc047615fb158719ffc93bddda5ce33c572
MaggieQI <23248016+StumbleMQ@users.noreply.github.com>
2025-01-08 11:34:51 +0800

    Update and rename Figure_8a.R to Supple.Figure_8a.R

--- Supple.Figure/Supple.Figure_8a/Figure_8a.R
+++
@@ -1,1 +0,0 @@
-### Please see the file of Figure_1d.R in the main figure fold 
---
+++ Supple.Figure/Supple.Figure_8a/Supple.Figure_8a.R
@@ -0,0 +1,9 @@
+### Please see the file of Figure_1d.R in main figure folder. 
+### Alternatively construct an IBD and Iron Metabolism geneset.gmt by downloading 
+### from https://www.genome.jp/dbget-bin/www_bget?pathway+hsa04216 & https://www.genome.jp/dbget-bin/www_bget?pathway+hsa05321 for GSEA analysis. 
+### load selected c2.cp.KEGG.symbols.gmt or custom IBD and Iron Metabolism.gmt file into Gene sets database
+### Number of permutations were set as 1000
+### load Selected Preranked files to Ranked List
+### Select collapsed to gene symbols
+### Select Human Gene_Symbol with Remapping _MSigDB as chip platform to obtain GSEA plots
+### Please see additional supplementary methods file for further information.
