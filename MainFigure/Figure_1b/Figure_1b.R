3d4727dbe599c63f870a9530cbabc7158bef58ef
MaggieQI <23248016+StumbleMQ@users.noreply.github.com>
2025-01-08 15:43:56 +0800

    Update Figure_1b.R

--- MainFigure/Figure_1b/Figure_1b.R
+++ MainFigure/Figure_1b/Figure_1b.R
@@ -1,1 +1,6 @@
-
+### The publicly available dataset for Figure 1b is obtained at the GEO database with accessions GSE53306
+### (https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE53306)
+###  Applying online tool GEO2R to Perform analysis with default settings
+### Download relevant specific genes expression profile by entering the corresponding identifier from the ID column of the Platform record.
+### Build a Figure_1b.xlsx datasheet for Figure 1b
+### End ###
